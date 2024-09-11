674ebfd67f5fc84cc240c8932e15ef80721460bc
jagdish-18 <shiyaljagdish18@gmail.com>
2024-09-11 10:19:02 +0530

    11-09-24

---
+++ model/user.model.js
@@ -0,0 +1,32 @@
+const mongoose  = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    firstName : {
+        type : String
+    },
+    lastName: {
+        type : String
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String
+    },
+    age : {
+        type : Number
+    },
+    mobileNo : {
+        type : String
+    },
+    profileImage : {
+        type : String
+    },
+    isDelete : {
+        type : Boolean,
+        default : false
+    }
+})
+
+module.exports = mongoose.model('users' , userSchema)
